0ee9e9e7a34948ad2bfb2ef812634df1e79163ac
Verzach3 <gabrielcorreacardenas@gmail.com>
2026-04-23 23:04:43 -0500

    fix: formatting

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,6 +2,7 @@ import { createClient } from "@libsql/client";
 import { env } from "@wellfit-emr/env/server";
 import { drizzle } from "drizzle-orm/libsql";
 
+// biome-ignore lint/performance/noNamespaceImport: We want to export all schema definitions under a single namespace for better organization.
 import * as schema from "./schema";
 
 export function createDb() {
